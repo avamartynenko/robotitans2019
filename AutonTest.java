ff9c38bfc3edd2bfa59cc1db4e680a006c7164f0
Ava Martynenko <capuccinowatermelon@gmail.com>
2019-10-09 19:24:22 -0400

    initial path

--- AutonTest.java
+++ AutonTest.java
@@ -80,6 +80,12 @@ public class AutonTest extends LinearOpMode {
     static final double DRIVE_SPEED = 0.6;
     static final double TURN_SPEED = 0.5;
 
+    static final int MOVE_PAUSE = 100; // delay between linear moves
+    static final double WALL_OVERRUN = 1; // home much father we run into the wall
+    static final double WALL_RECOIL = .5; // how far we pull back from the wall
+    static final double START_SPEED = .5; // highest possible speed with no slippage
+    static final double MAX_SPEED = 1;
+
     @Override
     public void runOpMode() {
 
@@ -92,12 +98,48 @@ public class AutonTest extends LinearOpMode {
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "init");    //
         telemetry.update();
-
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        if (opModeIsActive()) {
+        if (opModeIsActive())
+        {
+            // get cube
+            linearMoveWrapper(robot.FORWARD, 27, true); // runinto the wall
+            linearMoveWrapper(robot.RIGHT, 27, false);
+
+            // TODO: put get cube code here
+
+            // place cube
+            linearMoveWrapper(robot.LEFT, 28, true);
+            linearMoveWrapper(robot.REVERSE, 115, true);
+            sleep(100);
+
+            linearMoveWrapper(robot.RIGHT, 29, false);
+            linearMoveWrapper(robot.FORWARD,10, false);
+
+            // TODO: put place cube code here
+
+            // pullback and rotate
+            linearMoveWrapper(robot.LEFT, 4, false);
+            robot.linearMove(robot.GYRO_LEFT, MAX_SPEED, 21);
+            linearMoveWrapper(robot.REVERSE, 4, false);
+
+            // TODO: put grab platform code here
+
+            // pull platform back
+            linearMoveWrapper(robot.FORWARD, 32, false);
+
+            // push platform to the corner
+            linearMoveWrapper(robot.RIGHT, 27, false);
+            linearMoveWrapper(robot.REVERSE, 19, false);
+            linearMoveWrapper(robot.LEFT, 6, true);
+
+            // retreat and park under the bridge
+
+            linearMoveWrapper(robot.FORWARD, 19, true);
+            linearMoveWrapper(robot.RIGHT, 26, false);
+/*
+
             robot.linearMove(robot.FORWARD, 0.4, 18);
 
             sleep(5000);
@@ -123,21 +165,147 @@ public class AutonTest extends LinearOpMode {
             telemetry.addData("Path", "went back");
             telemetry.update();
 
-            robot.linearMove(robot.LEFT, 0.4, 46);
+            robot.linearMove(robot.LEFT, 0.4, 35);
 
             sleep(5000);
 
             telemetry.addData("Path", "went left");
-            telemetry.update();
+            telemetry.update();*/
 
+            //robot.linearMove(robot.FORWARD, 0.4, 15);
 
+            //sleep(10000);
 
+//            String strPath = "FORWARD .4:18~RIGHT  .4:73~REVERSE .4:15~LEFT .4:35";
 
+  //          followLinearPath(strPath, 5000);
 
             telemetry.addData("Path", "Complete :))))");
             telemetry.update();
         }
-
     }
 
+    public void linearMoveWrapper(int Direction, double Distance, boolean bHitWall)
+    {
+        // accelerate to max speed for first 5", drive at max speed, slow down
+
+        if(bHitWall)
+            Distance += WALL_OVERRUN;
+
+        robot.linearMove(Direction, MAX_SPEED, Distance);
+        sleep(bHitWall ? 0 : MOVE_PAUSE);
+        telemetry.addData("Path", "went left " + decodeDirection(Direction));
+    }
+
+    public void linearMoveWrapper2(int Direction, double Distance, boolean bHitWall)
+    {
+        // accelerate to max speed for first 5", drive at max speed, slow down
+
+        if(bHitWall)
+            Distance += WALL_OVERRUN;
+
+        // assuming 5 step acceleration
+        int accelerateSteps = (int)((Distance / 2) >= 5 ? 5 : (Distance / 2));
+
+        // accelerate
+        for(int i=0; i<accelerateSteps; i++)
+            robot.linearMove(Direction, START_SPEED + i*.1, 1);
+
+        // drive
+        if(Distance > 2*accelerateSteps)
+            robot.linearMove(Direction, MAX_SPEED, Distance - 2*accelerateSteps);
+
+        // slow down
+        for(int i=accelerateSteps; i>0; i--)
+            robot.linearMove(Direction, START_SPEED + i*.1, 1);
+
+        robot.linearMove(Direction, MAX_SPEED, Distance);
+
+        sleep(bHitWall ? 0 : MOVE_PAUSE);
+        telemetry.addData("Path", "went left " + decodeDirection(Direction));
+
+        if(bHitWall)
+        {
+            robot.linearMove(reverseDirection(Direction), MAX_SPEED/2, WALL_RECOIL);
+            sleep(MOVE_PAUSE/2);
+            telemetry.addData("Path", "aligned against the wall ");
+        }
+    }
+
+    public int reverseDirection(int Direction)
+    {
+        if(Direction == robot.LEFT)
+            return robot.RIGHT;
+        else if(Direction == robot.FORWARD)
+            return robot.REVERSE;
+        else if(Direction == robot.RIGHT)
+            return robot.LEFT;
+        else
+            return robot.FORWARD;
+    }
+
+    public String decodeDirection(int Direction)
+    {
+        if(Direction == robot.LEFT)
+            return "right";
+        else if(Direction == robot.FORWARD)
+            return "reverse";
+        else if(Direction == robot.RIGHT)
+            return "right";
+        else
+            return "forward";
+    }
+
+    // strPath format:
+    // Command Param|Command Param
+    public void followLinearPath(String strPath, int sleepInterval)
+    {
+        String[] strCommands = strPath.split("~");
+
+        for(int i=0; i<strCommands.length; i++)
+        {
+            String[] strCommand = strCommands[i].split(" ");
+            switch (strCommand[0])
+            {
+                case "FORWARD":
+                    linearMoveWrapper(robot.FORWARD, strCommand[1], sleepInterval);
+                    break;
+                case "REVERSE":
+                    linearMoveWrapper(robot.REVERSE, strCommand[1], sleepInterval);
+                    break;
+                case "LEFT":
+                    linearMoveWrapper(robot.LEFT, strCommand[1], sleepInterval);
+                    break;
+                case "RIGHT":
+                    linearMoveWrapper(robot.RIGHT, strCommand[1], sleepInterval);
+                    break;
+                default:
+                    //throw new Exception("Command not fouNd");
+                    break;
+            }
+        }
+    }
+
+    public void linearMoveWrapper(int intDirection, String strParams, int sleepInterval)
+    {
+        String[] strParam = strParams.split(":");
+        robot.linearMove(intDirection, Double.parseDouble(strParam[0]), Double.parseDouble(strParam[1]));
+
+        sleep(sleepInterval);
+
+        String strMessage;
+        strMessage = "Uninitialized direction. Check your path declaration";
+
+        if(intDirection == robot.FORWARD)
+            strMessage = "moved forward";
+        else if(intDirection == robot.LEFT)
+            strMessage = "moved left";
+        else if(intDirection == robot.RIGHT)
+            strMessage = "moved right";
+        else if(intDirection == robot.REVERSE)
+            strMessage = "moved back";
+
+        telemetry.addData("Path", strMessage);
+        telemetry.update();
+    }
 }
